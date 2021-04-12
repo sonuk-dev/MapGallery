96c25adacb50f5341401714b9fcc0b05ded4982b
Nune Gharibyan <gharibyannune1@gmail.com>
2021-04-12 17:45:20 +0400

    Add marker on image upload

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -14,27 +14,6 @@ export class MapComponent implements OnInit {
   lng = -122.41;
   zoom = 12
   constructor() { }
-  parseGPS(file) {
-
-    EXIF.getData(file, function () {
-      console.log(this)
-      let gpsLat = EXIF.getTag(this, "GPSLatitude");
-      let gpsLng = EXIF.getTag(this, "GPSLongitude");
-      const latDec = gpsLat[0] + gpsLat[1] / 60 + gpsLat[2] / 3600;
-      const lngDec = gpsLng[0] + gpsLng[1] / 60 + gpsLng[2] / 3600;
-      let latRef = EXIF.getTag(this, "GPSLatitudeRef");
-      let lngRef = EXIF.getTag(this, "GPSLongitudeRef");
-      if (latRef == 'S') {
-        latRef *= -1;
-      }
-      if (lngRef == 'W') {
-        lngRef *= -1;
-      }
-      console.log(latDec)
-      console.log(lngDec)
-    });
-
-  }
   ngOnInit() {
     this.map = new mapboxgl.Map({
       accessToken: environment.mapbox.accessToken,
@@ -45,5 +24,37 @@ export class MapComponent implements OnInit {
     });
     // Add map controls
     this.map.addControl(new mapboxgl.NavigationControl());
+  }
+  parseGPS(file) {
+    let env = this;
+    EXIF.getData(file, function () {
+      console.log(this)
+      let gpsLat = EXIF.getTag(this, "GPSLatitude");
+      let gpsLng = EXIF.getTag(this, "GPSLongitude");
+      let latDec = gpsLat[0] + gpsLat[1] / 60 + gpsLat[2] / 3600;
+      let lngDec = gpsLng[0] + gpsLng[1] / 60 + gpsLng[2] / 3600;
+      let latRef = EXIF.getTag(this, "GPSLatitudeRef");
+      let lngRef = EXIF.getTag(this, "GPSLongitudeRef");
+      if (latRef == 'S') {
+        latDec *= -1;
+      }
+      if (lngRef == 'W') {
+        lngDec *= -1;
+      }
+      console.log(latDec)
+      console.log(lngDec)
+      env.addMarker(latDec, lngDec)
+      env.map.flyTo({
+        center: [lngDec, latDec]
+        });
+    });
+  }
+  addMarker(lat, lng){
+      let marker = new mapboxgl.Marker({
+      // color: "#FFFFFF",
+      draggable: true,
+      pitchAlignment: "auto",
+      }).setLngLat([lng, lat])
+      .addTo(this.map);
   }
 }
