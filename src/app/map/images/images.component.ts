091da76ddde4b83e2112efd286a5c9e8d9636c6d
Nune Gharibyan <2003sonag@gmail.com>
2021-05-17 20:50:59 +0400

    updated image component

--- src/app/map/images/images.component.ts
+++ src/app/map/images/images.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, HostListener, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 import { MapService } from "../map.service";
-import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-images',
@@ -8,33 +9,16 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./images.component.scss']
 })
 export class ImagesComponent {
-  constructor(public mapService: MapService, private sanitizer: DomSanitizer) { }
-  images = [];
-  ngOnInit(): void {
-    this.getImages();
-  }
-  getImages() {
-    this.mapService.getImages().subscribe(
-      (res: any) => {
-        console.log(res)
-        for (let i = res.images.length - 1; i > -1; i--) {
-          let image = this._arrayBufferToBase64(res.images[i].data.data);
-          this.images.push(this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;base64, ${image}`));
-        }
-      },
-      err => console.log(err)
-    );
-  }
-  _arrayBufferToBase64(buffer) {
-    let binary = '';
-    let bytes = new Uint8Array(buffer);
-    let len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-      binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
-  }
-  image(){
 
+  constructor(public mapService: MapService, private router: Router) { }
+  ngOnInit(): void {
+    this.mapService.getImages();
+  }
+
+  image(event, index) {
+    console.log(index)
+    this.mapService.imageIndex.next(index);
+    // this.showMarker.emit(index);
+    this.router.navigate(['/map/description', { src: event.target.currentSrc }]);
   }
 }
